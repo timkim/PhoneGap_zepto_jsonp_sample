0d5860e8d1e3fa86190b19507e077c76aab9c6f2
Tim Kim <tim.kim@nitobi.com>
2011-07-19 17:36:20 -0700

    Changed some file names around

--- server/simplejsonp.php
+++ server/simplejsonp.php
@@ -1,5 +1,4 @@
 <?php header('content-type: application/json; charset=utf-8');
-$twitterSearch = $_GET["twitterSearch"];
 $data = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
 echo $_GET['callback'].'('.json_encode($data).')';
 ?>
